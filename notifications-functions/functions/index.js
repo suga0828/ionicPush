802a6eac1f704bb374ce2da4fb8a3ba2190a04d8
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-08-03 12:41:25 -0500

    Create firebase project and first function with Javascript

---
+++ notifications-functions/functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+const admin = require('firebase-admin');
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+const app = express();
+app.use(cors());
+
+admin.initializeApp();
+
+app.post('/', async (req, res) => {
+  let token = req.body.token;
+  try {
+    let response = await admin.messaging.send({
+      notification: {
+        title: 'Primera notificación',
+        body: ''
+      },
+      token: token
+    });
+
+    return res.json(response);
+  } catch(err) {
+    return res.json(err).status(500);
+  }
+});
+
+exports.notification = functions.https.onRequest(app);
